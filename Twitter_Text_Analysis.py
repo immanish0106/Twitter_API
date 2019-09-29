b73792cd127701bab50b2b43f17548038e1766bd
immanish0106 <immanish0106@gmail.com>
2019-09-30 02:32:16 +0530

    Add files via upload

---
+++ Twitter_Text_Analysis.py
@@ -0,0 +1,25 @@
+# Import packages
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot histogram
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
+
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders +=word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
